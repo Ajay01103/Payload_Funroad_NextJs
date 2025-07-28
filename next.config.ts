e8c365323292aab55103593ec20ed7d06da85ae9
Ajay01103 <125455606+Ajay01103@users.noreply.github.com>
2025-07-28 19:19:03 +0530

    payload_integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+/** biome-ignore-all lint/style/useNodejsImportProtocol: Fine to not use node:path */
+import { withPayload } from "@payloadcms/next/withPayload"
 import type { NextConfig } from "next"
+import path from "path"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
 }
 
-export default nextConfig
+export default withPayload(nextConfig)
